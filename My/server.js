0ccc04a3c82d09e1b857dbd38e511fa7ae1f4e0c
loganoe <logankento@outlook.com>
2025-02-22 11:17:57 -0800

    Travel Update

--- My/server.js
+++ My/server.js
@@ -1,21 +1,185 @@
 const express = require("express");
 const cors = require("cors");
-
-
-
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
+const https = require("https");
 
+
+
+// Use dynamic import for node-fetch
+let fetch;
+async function loadFetch() {
+    fetch = (await import('node-fetch')).default; // dynamically import node-fetch
+}
+loadFetch(); // Call it to initialize fetch
+async function triggerActionInBackend() {
+    console.log('Backend function triggered!');
+
+    // Send a request to the correct endpoint (use https://localhost:3001)
+    fetch('https://localhost:3001/trigger-action', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        agent: new https.Agent({
+            rejectUnauthorized: false // Disable certificate validation for self-signed certificates
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message); // Log the response from the frontend (via response)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Create an HTTPS server
+
+
+var espIp = "http://192.168.68.173";
 const app = express();
-const PORT = 3000;
-app.use(cors({ origin: "http://localhost:3000" }));
+const PORT = 3001;
+
+// Functions to control the robot
+async function moveForward() { 
+    await fetch(espIp + '/forward'); 
+}
+
+async function moveLeft() { 
+    await fetch(espIp + '/left'); 
+}
+
+async function stopRobot() { 
+    await fetch(espIp + '/stop'); 
+}
+
+async function moveRight() { 
+    await fetch(espIp + '/right'); 
+}
+
+async function moveReverse() { 
+    await fetch(espIp + '/reverse'); 
+}
+
+async function moveUp() { 
+    await fetch(espIp + '/up'); 
+}
+
+async function stopArm() { 
+    await fetch(espIp + '/stoparm'); 
+}
+
+async function moveDown() { 
+    await fetch(espIp + '/down'); 
+}
+
+// Function to extract and handle movement commands
+async function extractMovement(string) {
+    if (string.includes("{")) {
+    const extractTextBetweenSubstrings = (str, start, end) => {
+        const startIndex = str.indexOf(start) + start.length;
+        const endIndex = str.indexOf(end, startIndex);
+        return str.slice(startIndex, endIndex);
+    };
+
+    var newTxt = extractTextBetweenSubstrings(string, "{", "}");
+    var possibleCombs = newTxt.split(",");
+    
+    for (let i = 0; i < possibleCombs.length; i++) {
+        if (possibleCombs[i].includes("forward")) {
+            console.log("NEW COMMAND RECEIVED! 1");
+            var dStr = possibleCombs[i].replace("forward:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor((tStr * 1000) / 3.8);
+            console.log(time);
+            await moveForward();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopRobot();
+        } else if (possibleCombs[i].includes("backward")) {
+            console.log("NEW COMMAND RECEIVED! 2");
+            var dStr = possibleCombs[i].replace("backward:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor((tStr * 1000) / 3.8);
+            console.log(time);
+            await moveReverse();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopRobot();
+        } else if (possibleCombs[i].includes("right")) {
+            console.log("NEW COMMAND RECEIVED! 3");
+            var dStr = possibleCombs[i].replace("right:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor(tStr * 25.5);
+            console.log(time);
+            await moveRight();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopRobot();
+        } else if (possibleCombs[i].includes("left")) {
+            console.log("NEW COMMAND RECEIVED! 4");
+            var dStr = possibleCombs[i].replace("left:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor(tStr * 25.5);
+            console.log(time);
+            await moveLeft();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopRobot();
+        } else if (possibleCombs[i].includes("up")) {
+            console.log("NEW COMMAND RECEIVED! 5");
+            var dStr = possibleCombs[i].replace("up:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor(tStr * 20);
+            console.log(time);
+            await moveUp();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopArm();
+        } else if (possibleCombs[i].includes("down")) {
+            console.log("NEW COMMAND RECEIVED! 6");
+            var dStr = possibleCombs[i].replace("down:", "");
+            var tStr = Number(dStr);
+            var time = Math.floor(tStr * 20);
+            console.log(time);
+            await moveDown();
+            await new Promise(resolve => setTimeout(resolve, time));
+            await stopArm();
+        } else if (possibleCombs[i].includes("goToObject")) {
+            console.log("NEW COMMAND RECEIVED! 7");
+            var dStr = possibleCombs[i].replace("goToObject:", "");
+            var tStr = dStr;
+            homeToItem(tStr);
+        }
+    }
+    console.log("processing finish exception")
+
+} else {
+    console.log("processing finish exception")
+    if (string.includes("PERSISTENT")) {
+        // I want to do the same thing as the /analyze route, but with new image data. 
+        console.log("Calling the AI again with new image data...");
+        triggerActionInBackend();
+    } else {
+
+    }
+    
+}
+}
+// configure middleware
+app.use(cors({ origin: "https://localhost:3001" })); // note the 'https' protocol
 app.use(bodyParser.json({ limit: "10mb" }));
 
 // fixed system prompt
-const SYSTEM_PROMPT = "You are in control of a PHYSICAL ROBOT which can move forward and backward, as well as turn a specified amount of degrees. If you think it is appropriate based on the user's prompt, you can move the robot forward, backward, or turn it by putting a command into curly brackets {}, in a json-like manner. Note that, unlike json, the commands, e.g. forward, and the numbers do not need to be encased in quotation marks. For example, to move forward 10cm, just say {forward: 10}. To move left 90 degrees, say {left: 90}. THE UNITS FOR TURNING ARE IN DEGREES, AND THE UNITS FOR MOVEMENT ARE IN CENTIMETERS. You can also not give any commands and only give a text answer, if it is appropriate. Text answers as well as movement commands are also permitted. Putting multiple movement commands separated by commas is also fine, and they will be executed sequentially in left to right order (e.g., to move 90 degrees left then forward 10 cm, the command would be {left:90,forward:10}). No more than one set of curly brackets is allowed NO MATTER WHAT, EVEN IN EXAMPLES. Curly brackets will be interpreted as movement commands, even if they are in quotation marks or used as an example. For example, if you wanted to have a long string of movements, you would use {left:90, forward: 10, right: 180, backward: 20, left: 100, forward: 5}, all in a single curly bracket. In addition, the robot has a 15 cm arm on the front with a magnet on the end. To move the arm down 10 degrees, use {down: 10}. To move the arm up 10 degrees, use {up: 10}. These commands follow the same rules as all other commands, and are always measured in degrees. They can be combined with robot movement. By defualt, the arm is vertical (pointing up), and to move it fully down, it is recommended to move it down around 240 degrees. When the user asks you to pick up an item, move the arm down after you are directly over the item. Otherwise, you could accidentally push other stuff. Also, when the user asks you to pick something up, move the arm down 240 degrees to ensure proper contact with the object. Make sure to move the arm back up 240 degrees after grabbing the item. After using the arm, remember to bring it back up to the veritcal position. Additionally, the Robot has 2 stationary rails extruding from the front left and front right of the vehicle, which can be used to align bottles, cans, or other items while being pushed. The rails should only be used when the front robot arm is all the way up. You can ensure this by running the command {up: 150} before attempting to push an item. The rails don't pick an object up, they just surround the object with a U shape, and the object has no support on the bottom. Also, REMEMBER THAT MOVING BACKWARD WHILE USING THE RAILS WILL NOT MOVE THE OBJECT BEING PUSHED, SO TO MOVE BACKWARDS, TURN AROUND AND THEN MOVE FORWARDS. ADDITIONALLY, TO ACCOUNT FOR WHEEL SLIPPAGE WHILE TURNING WITH HEAVY OBJECTS, MULTIPLY ALL TURN VALUES BY 2.2 WHEN USING THE RAILS! IF NOT USING THE RAILS, MULTIPLY ALL TURN VALUES BY 1.8! DO THE TURN CALCULATION BEFORE PUTTING IT INTO THE CURLY BRACKETS, AS THE CURLY BRACKETS ONLY ACCEPT FLOATS, WITHOUT PERMITTING ANY ARITHMETIC INSIDE OF THEM.  WHILE USING THE RAILS, REMEMBER THAT THE ARM DOES NOT HAVE TO BE PUSHED DOWN TO PICK UP OBJECTS, AS THEY ARE PUSHED, NOT LIFTED. WHEN TRYING TO MOVE A BOTTLE, CAN, OR OTHER HEAVY OBJECT, USE THE RAILS, NOT THE ARM!!! THE ARM IS ONLY TO BE USED FOR LIGHT, SHORT OBJECTS. A HEAVY OBJECT IS ANY OBJECT OVER AROUND 50 GRAMS, SO ALL CANS, BOTTLES, ETC SHOULD BE MOVED USING THE RAILS. ALSO, REMEMBER THAT THE ARM CANNOT PICK UP OBJECTS THAT ARE NOT MADE OF METAL. UNDER NO CIRCUMSTANCES ARE YOU TO EVER RESPOND WITH MORE THAN 1 SET OF CURLY BRACKETS IN THE ENTIRE MESSAGE, EVEN IF IT IS SEPARATED BY COMMAS OR OTHER WORDS. THIS WILL BREAK THE ROBOT SYSTEM AND NECESSITATE A RESTART OF THE SERVERS. EVEN IF MAKING A LIST OF INSTRUCTIONS, PUT ALL OF THE COMMANDS INTO A SINGLE SET OF CURLY BRACKETS SEPERATED BY COMMAS, AND DON'T EVER USE MORE THAN ONE SET OF CURLY BRACKETS (MEANING THAT THERE SHOULD ONLY BE A SINGLE SET OF CURLY BRACKETS IN THE ENTIRE RESPONSE) !!! Note that all text you output will be displayed and parsed as plaintext, so if you have curly brackets anywhere in yoru response, even if it is not in json format, it will be considered as a movement command, and there can only be strictly one of them in the entier response. Your job is to do what is appropriate given the user's prompt, and give a clear, consice answer to any questions or tasks asked of you, while following the guidelines given to you prior to this. Additionally, attached to the users prompt will be a list of all the objects in the robot's field of view. If the user asks to get one of these objects, you can also run an algorithm to get the object and then return to the starting position, which can be invoked with getItem: <item_name>. For example, if the user wanted you to move forward 10 cm, grab a can, then move forward 10 cm again, the command would look like {forward: 10, getItem: can, forward: 10}. It is highly encouraged that you use the algorithm for grabbing objects when possible, as the algorithm is able to ensure alignment and use computer vision to get the object. If the object is in the list provided, it will automatically use the rails to grab the object. In summary, IF THE USER ASKS YOU TO GET AN OBJECT THAT IS ON THE OBJECT LIST PROVIDED, USE THE getItem ALGORITHM. You do not need any commands other than the getItem algorithm to get an item, as it will automatically raise the arm, get the item, and come back. The camera you see the world through is mounted at the top of the robot, and is an ultrawide camera. ";
+const SYSTEM_PROMPT = `You are a PHYSICAL ROBOT with the ability to move and interact with the world. Use simple, brief responses for speech. Your movements are controlled with commands in curly brackets, such as {forward: 10} for 10 cm forward or {left: 90} for a 90° turn. Use one set of curly brackets for all movement commands, even if multiple actions are needed (e.g., {left: 90, forward: 10}). You can also not add any movement commands and just provide a text response.
 
+You have a 15 cm arm with a magnet. Move the arm down 240° to pick up items, and bring it back up after use. The arm can only handle light objects, while heavy items (over 50g) should be moved using the rails, which can push items but not lift them.
+
+When a task requires continuous action, append "PERSISTENT" to your response. For example, moving forward until an object is detected should be phrased as: “Move forward 10 cm” + “PERSISTENT” to indicate ongoing action until the task completes. Remove the "PERSISTENT" tag when the task is finished.
+
+To calculate turns:
+
+Use a multiplier of 2.2 for turns when using the rails.
+Use 1.8 for all other turns. Ensure you always use a single set of curly brackets for any instructions, as multiple sets will break the system. Avoid any commands that aren't {forward}, {backward}, {left}, {right}, {up}, or {down}, and remember to add the appropriate numbers for each movement.`;
+var prePrompt = ""; 
 // handle image analysis request
 app.post("/analyze", async (req, res) => {
     try {
@@ -24,22 +188,26 @@ app.post("/analyze", async (req, res) => {
         // decode base64 image
         const imagePath = path.join(__dirname, "input.jpg");
         fs.writeFileSync(imagePath, Buffer.from(image, "base64"));
-
+        console.log(imagePath); 
         // send to ollama
         const response = await axios.post("http://localhost:11434/api/generate", {
-            model: "llava:13b",
-            prompt: `${SYSTEM_PROMPT}\nUser: ${prompt}`,
+            model: "llama3.2-vision",
+            prompt: `${SYSTEM_PROMPT}\nUser: ${prompt + ". REMEMBER TO ADD PERSISTENT TO THE END OF THE AI RESPONSE IF YOU THINK THE USER'S REQUEST HAS NOT BEEN SATISFIED."}`,
             images: [fs.readFileSync(imagePath, "base64")], // send image as base64
             stream: false
         });
-        console.log("User request sent: ", prompt); 
+        console.log("User request sent: ", prompt);
+        prePrompt = prompt; 
         console.log(response.data.response);
+
         res.json({ reply: response.data.response });
+        await extractMovement(response.data.response);
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: "Failed to process image" });
     }
 });
+
 // serve the HTML file for the chat UI
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/ui.html'));
@@ -49,4 +217,52 @@ app.get('/', function (req, res) {
 app.get('/visionxv', function (req, res) {
     res.sendFile(path.join(__dirname, '/visionxv.html'));
 });
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.post('/trigger-action', (req, res) => {
+    const response = triggerActionInBackend(); // Trigger backend action
+    res.json(response);  // Send the response to the frontend
+  });
+// read the ssl certificate and key
+const sslOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+
+// Function to trigger action in the frontend
+function triggerActionInBackend() {
+    console.log('Backend function triggered!');
+    
+    // Send an SSE message to the frontend
+    broadcastToClients(prePrompt + ". THIS IS A PERSISTENT COMMAND. ADD PERSISTENT TO THE END OF YOUR NEXT MESSAGE IF YOU THINK THE USER'S REQUEST HAS NOT BEEN SATISFIED. IF THE USER REQUEST HAS BEEN SATISFIED, REMOVE THE PERSISTENT TAG.");
+}
+
+
+const clients = [];
+// SSE route to send events to the frontend
+app.get("/events", (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders(); // Flush headers to establish the SSE connection
+
+    // Add the client to a list of connected clients
+    clients.push(res);
+
+    // Remove the client when the connection is closed
+    req.on('close', () => {
+        const index = clients.indexOf(res);
+        if (index !== -1) {
+            clients.splice(index, 1);
+        }
+    });
+});
+// Function to broadcast messages to all connected clients
+function broadcastToClients(message) {
+    clients.forEach(client => {
+        client.write(`data: ${message}\n\n`);
+    });
+}
+// create an https server
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`Server running on https://localhost:${PORT}`);
+});
